5111866783971436b2b92193bb1984d7745f9264
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-15 12:18:32 -0500

    contacts app and model created and migrtions ran

--- btre/settings.py
+++ btre/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    
 ]
 
 MIDDLEWARE = [
