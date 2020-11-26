fcbb345cec08d54ad422f975c3d9fedda4ef8461
SamKlep <shamdemann23@gmail.com>
2020-11-25 22:13:10 -0600

    Logging.

--- btre/settings.py
+++ btre/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import django_heroku
 from django.contrib.messages import constants as messages
 import os
-
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
