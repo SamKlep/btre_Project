fc5f641f2bf09920d2f76c2c883787ad02d4aa12
SamKlep <shamdemann23@gmail.com>
2020-11-25 21:55:22 -0600

    Heroku.

--- btre/settings.py
+++ btre/settings.py
@@ -129,7 +129,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
