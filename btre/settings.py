82a038c449c0fd2648553abc41fc9ec4e3b3b9ec
SamKlep <shamdemann23@gmail.com>
2020-11-25 22:27:56 -0600

    Hosts.

--- btre/settings.py
+++ btre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$nxc2wt1ijtkubv73-7((4od#+zk9x+!h95v69!tfqxm_wp%ys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-house.herokuapp.com']
 
